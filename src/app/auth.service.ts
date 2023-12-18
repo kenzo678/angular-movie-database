fa46450860cc11b1eb06c6e2b19bcf47fa26811e
kenzp <kenzoshimabukuro01@gmail.com>
2023-12-18 13:16:53 -0400

    Dockerfile y build-app.sh. No funciona el dockerfile con la API para ver las peliculas en la webapp. Arreglar.

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -3,10 +3,24 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 
+import { catchError, map } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+import { SHA256 } from 'crypto-js';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  private hashUsername(username: string): string {
+    return SHA256(username).toString();
+  }
+
+  private hashPassword(password: string): string {
+    return SHA256(password).toString();
+  }
+
   private isAuthenticatedSubject = new BehaviorSubject<boolean>(false);
 
   get isAuthenticated$(): Observable<boolean> {
@@ -16,7 +30,7 @@ export class AuthService {
   get isAuthenticated(): boolean {
     return this.isAuthenticatedSubject.value;
   }
-
+/*
   login(username: string, password: string): boolean {
     //encriptar despues;
     const isValid = username === 'kenzo' && password === 'contra123';
@@ -27,6 +41,29 @@ export class AuthService {
 
     return isValid;
   }
+*/
+
+login(username: string, password: string): boolean {
+  const storedUsername = 'testuser';
+  const storedPassword = 'testpass';
+
+  // Hash the values
+  const storedHashedUsername = this.hashUsername(storedUsername);
+  const storedHashedPassword = this.hashPassword(storedPassword);
+
+  const enteredHashedUsername = this.hashUsername(username);
+  const enteredHashedPassword = this.hashPassword(password);
+
+  if (enteredHashedPassword === storedHashedPassword && enteredHashedUsername === storedHashedUsername) {
+    // Simulate successful login
+    this.isAuthenticatedSubject.next(true);
+    return true;
+    //return { success: true };
+  } else {
+    return false;
+    //return { success: false, message: 'Invalid credentials' };
+  }
+}
 
   logout() {
     console.log('Logout method called');
